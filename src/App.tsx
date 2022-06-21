7a4de808a97541a944044b21eacc933cadd04a61
artFetisov <fetishfestoff@mail.ru>
2022-06-21 14:53:44 +0300

    update App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import {User} from "./components/user/User";
 
 function App() {
   return (
     <div className="App">
       start project
+        <User />
     </div>
   );
 }
